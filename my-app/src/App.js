6914ba890b09025fe905b9f67ed6bf170845cb54
do12ak <93183532+do12ak@users.noreply.github.com>
2021-11-06 14:40:19 +0900

    入力値の保持

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
 function App() {
+  const[value,setValue] = React.useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+          <form>
+            <input
+             value={value}
+             onChange={event => setValue(event.target.value)} />
+          </form>
+      <div>
+        {value}
+      </div>
     </div>
   );
 }
