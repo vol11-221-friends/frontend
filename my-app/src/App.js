6e04b0db835fa790fbc8ca2085ae3ad846dd963c
do12ak <93183532+do12ak@users.noreply.github.com>
2021-11-06 18:03:05 +0900

    axiosでpost

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react"
+import React, {useState} from "react"
+import axios from "axios";
+
 
 function App() {
   const[value,setValue] = React.useState("");
+  var obj = {}
+  const json = (text) =>{
+    setValue(text)
+    obj={"name":text}
+    console.log(obj)
+  }
+
+  const tuusin = () =>{
+    const res = axios.post('/user', obj)
+    console.log(res);
+  }
 
   return (
     <div className="App">
@@ -11,13 +24,19 @@ function App() {
           <form>
             <input
              value={value}
-             onChange={event => setValue(event.target.value)} />
+             onChange = {event => json(event.target.value)}  />
+             <input type="button" value="(submit)" onClick={tuusin} /> 
           </form>
-      <div>
-        {value}
-      </div>
+
     </div>
   );
 }
 
 export default App;
+
+/*function json(value){
+  setValue(value)
+  var obj = {"name":value}
+  var json_str = JSON.stringify(obj)
+  console.log(json_str)
+}*/
